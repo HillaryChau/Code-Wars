2f6b4be9eec8b8c01754c14e596e0c067f27d8e7
Hillary Chau <77407356+HillaryChau@users.noreply.github.com>
2021-02-22 19:17:50 -0500

    Create 8SumOfPositive.js

---
+++ 8kyu/8SumOfPositive.js
@@ -0,0 +1,21 @@
+/*
+Sum of Positive
+You get an array of numbers, return the sum of all of the positives ones.
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+function positiveSum(arr) {
+  let neg = []
+  let sum = 0
+
+  for (let i=0; i < arr.length; i++){
+    if (arr[i] < 0){
+      neg.push(arr[i]);
+    }else{
+      sum+= arr [i];
+    }
+  }
+  return sum
+}
