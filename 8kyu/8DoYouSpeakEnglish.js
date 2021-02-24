11ba036defcb20f13200c07f5f5ac3692841c786
Hillary Chau <hillarychau8@gmail.com>
2021-02-24 14:00:00 -0500

    add do you speak english

---
+++ 8kyu/8DoYouSpeakEnglish.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/train/javascript
+
+
+function spEng(sentence) {
+  //write your code here
+  if (sentence.toLowerCase().includes("english")) {
+    return true;
+  } else {
+    return false;
+  }
+}
