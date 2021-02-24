880e198940303189940120497cfa9af519aba399
Hillary Chau <77407356+HillaryChau@users.noreply.github.com>
2021-02-23 20:34:17 -0500

    Create 8NumberToString.js
    used function.toString method to convert number to string

---
+++ 8kyu/8NumberToString.js
@@ -0,0 +1,23 @@
+/*
+
+Convert a Number to a String!
+
+We need a function that can transform a number into a string.
+What ways of achieving this do you know?
+
+*/
+
+function numberToString(num) {
+  // Return a string of the number here! 
+  return num.toString()
+}
+
+
+
+/*another method is 
+
+function numberToString(num) {
+     return String(num)
+}
+
+*/
