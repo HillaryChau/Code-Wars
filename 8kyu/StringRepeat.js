02a5a5a589cff76d64e9d18e0db34321e744bd56
Hillary Chau <77407356+HillaryChau@users.noreply.github.com>
2021-03-01 00:55:50 -0500

    Create StringRepeat.js

---
+++ 8kyu/StringRepeat.js
@@ -0,0 +1,15 @@
+
+/*
+Write a function called repeat_str which repeats the given string src exactly count times.
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+*/
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+
+//or try this solution// 
+
+repeatStr = (n, s) => s.repeat(n)
