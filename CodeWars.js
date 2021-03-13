b33bd3c5d42f9dd5bcdfcfdc737861b793466ac3
Hillary Chau <77407356+HillaryChau@users.noreply.github.com>
2021-03-12 22:42:24 -0500

    Update CodeWars.js

--- CodeWars.js
+++ CodeWars.js
@@ -1,7 +1,7 @@
 //These are my Code Wars //
 
 //8- Return Negative//
-//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?//
+//In this simple assignment you are given a number and have to make it negative.  But maybe the number is already negative?//
 
 function makeNegative(num) {
   if (num>0){
