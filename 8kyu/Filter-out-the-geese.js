1500b10c3e231ea72823e0b431c877b4bbde831d
Hillary Chau <77407356+HillaryChau@users.noreply.github.com>
2021-04-09 23:27:42 -0400

    Update Filter-out-the-geese.js

--- 8kyu/Filter-out-the-geese.js
+++ 8kyu/Filter-out-the-geese.js
@@ -19,3 +19,14 @@ The elements in the returned array should be in the same order as in the initial
 FUNDAMENTALSHIGHER-ORDER FUNCTIONSFUNCTIONSCONTROL FLOWBASIC LANGUAGE FEATURESARRAYS
 
 */
+
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  
+  // return an array containing all of the strings in the input array except those that match strings in geese
+  
+   return birds.filter(bird => !geese.includes(bird))
+  
+};
+
+
