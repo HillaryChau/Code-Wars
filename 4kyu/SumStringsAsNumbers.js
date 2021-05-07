3a9362e74dc39647ebdf1b6bf792db488a04c105
Hillary Chau <77407356+HillaryChau@users.noreply.github.com>
2021-05-06 22:17:11 -0400

    Create SumStringsAsNumbers.js

---
+++ 4kyu/SumStringsAsNumbers.js
@@ -0,0 +1,15 @@
+/*https://www.codewars.com/kata/5324945e2ece5e1f32000370
+Given the string representations of two integers, return the string representation of the sum of those integers.
+
+For example:
+
+sumStrings('1','2') // => '3'
+A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+
+Tags: ALGORITHMS STRINGS ARITHMETIC MATHEMATICS NUMBERS BIGINTEGERS INTEGERS UTILITIES
+*/
+
+function sumStrings(a,b) { 
+  let newStr = BigInt(a) + BigInt(b)
+  return newStr.toString()
+}
